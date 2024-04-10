81762c5de0e9f1582e713102ca703e1762dea30f
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-04-10 14:20:52 -0300

    fixes

--- Interpolation/Lagrange.py
+++ Interpolation/Lagrange.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+
 def Lagrange():
     print("How many points do you have?")
     n = int(input())
@@ -8,49 +12,44 @@ def Lagrange():
         x.append(float(input()))
         print("Enter the f(x)/y value of the point", i+1)
         y.append(float(input()))
-        
+
     print("Do you want the formula or the value of the function?")
     print("1. Formula")
     print("2. Value")
     print("3. Both")
     print("--> ")
     choice = int(input())
-    if (choice == 1):
+
+    X = sp.symbols('x')
+    lagrange_poly = 0
+
+    for i in range(n):
+        p = 1
+        for j in range(n):
+            if i != j:
+                p *= (X - x[j]) / (x[i] - x[j])
+        lagrange_poly += y[i] * p
+
+    if choice == 1 or choice == 3:
         print("The formula is:")
-        for i in range(n):
-            p = ""
-            for j in range(n):
-                if (i != j):
-                    p += "(x - " + str(x[j]) + ")/(" + str(x[i]) + " - " + str(x[j]) + ")"
-            print(str(y[i]) + " * " + p)
-    elif (choice == 2):
-        print("The formula is:")
-        for i in range(n):
-            p = ""
-            for j in range(n):
-                if (i != j):
-                    p += "(x - " + str(x[j]) + ")/(" + str(x[i]) + " - " + str(x[j]) + ")"
-            print(str(y[i]) + " * " + p)
-            
+        print(sp.simplify(lagrange_poly))
+
+    if choice == 2 or choice == 3:
         print("Enter the value of x")
         x0 = float(input())
-        result = 0
-        for i in range(n):
-            p = 1
-            for j in range(n):
-                if (i != j):
-                    p *= (x0 - x[j])/(x[i] - x[j])
-            result += y[i] * p
+        result = lagrange_poly.subs(X, x0)
         print("The value of the function at x =", x0, "is", result)
-        
-    else:
-        print("Enter the value of x")
-        x0 = float(input())
-        result = 0
-        for i in range(n):
-            p = 1
-            for j in range(n):
-                if (i != j):
-                    p *= (x0 - x[j])/(x[i] - x[j])
-            result += y[i] * p
-        print("The value of the function at x =", x0, "is", result)
+
+    # Plotting
+    x_vals = np.linspace(min(x), max(x), 400)
+    y_vals = [lagrange_poly.subs(X, val) for val in x_vals]
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(x_vals, y_vals, label='Lagrange Polynomial')
+    plt.scatter(x, y, color='red')
+    plt.title('Lagrange Interpolation')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
--- Interpolation/Newton.py
+++ Interpolation/Newton.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+
 def Newton():
     print("How many points do you have?")
     n = int(input())
@@ -8,48 +12,46 @@ def Newton():
         x.append(float(input()))
         print("Enter the f(x)/y value of the point", i+1)
         y.append(float(input()))
-        
+
     print("Do you want the formula or the value of the function?")
     print("1. Formula")
     print("2. Value")
     print("3. Both")
     print("--> ")
     choice = int(input())
-    
-    if (choice == 1):
+
+    X = sp.symbols('x')
+    newton_poly = 0
+    div_diff = y.copy()
+
+    for i in range(n):
+        term = div_diff[i]
+        for j in range(i):
+            term *= (X - x[j])
+        newton_poly += term
+        for j in range(n - i - 1):
+            div_diff[j] = (div_diff[j + 1] - div_diff[j]) / (x[j + i + 1] - x[j])
+
+    if choice == 1 or choice == 3:
         print("The formula is:")
-        print(y[0])
-        for i in range(1, n):
-            p = ""
-            for j in range(i):
-                p += "(x - " + str(x[j]) + ")"
-            print(" + " + str(y[i]) + " * " + p)
-    elif (choice == 2):
-        print("The formula is:")
-        print(y[0])
-        for i in range(1, n):
-            p = ""
-            for j in range(i):
-                p += "(x - " + str(x[j]) + ")"
-            print(" + " + str(y[i]) + " * " + p)
-            
+        print(sp.simplify(newton_poly))
+
+    if choice == 2 or choice == 3:
         print("Enter the value of x")
         x0 = float(input())
-        result = y[0]
-        for i in range(1, n):
-            p = 1
-            for j in range(i):
-                p *= (x0 - x[j])
-            result += y[i] * p
+        result = newton_poly.subs(X, x0)
         print("The value of the function at x =", x0, "is", result)
-    else:
-        print("Enter the value of x")
-        x0 = float(input())
-        result = y[0]
-        for i in range(1, n):
-            p = 1
-            for j in range(i):
-                p *= (x0 - x[j])
-            result += y[i] * p
-        print("The value of the function at x =", x0, "is", result)
-        
+
+    # Plotting
+    x_vals = np.linspace(min(x), max(x), 400)
+    y_vals = [newton_poly.subs(X, val) for val in x_vals]
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(x_vals, y_vals, label='Newton Polynomial')
+    plt.scatter(x, y, color='red')
+    plt.title('Newton Interpolation')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
